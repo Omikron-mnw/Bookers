afa43c69902cd6135390bd7b4c1e53ccd24ba3bf
mnwhryk <minomushi.ff15.07.08@gmail.com>
2020-11-11 08:26:15 +0000

    2 commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,53 +1,40 @@
 class BooksController < ApplicationController
-  def index
+
+  def top
+    @book =
+
+   index
+    @book = Book.all
   end
 
   def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
-end
-
-
-
-
-
-
- def show
     @book = Book.find(params[:id])
-
-  def new
-    @book = Book.new(book_params)
   end
 
   def create
     book = Book.new
     book.save
     redirect_to books_path(book.id)
+  end
 
-  def index
-    @book = Book.all
+  def new
+    @book = Book.new(book_params)
   end
 
   def edit
-    @book = Book.find(params[:id])
+     @book = Book.find(params[:id])
   end
 
   def uodate
     @book = Book.find(params[:id])
     blog.update(book_params)
     redirect_to bookl_path(book.id)
-
+  end
 
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
 
-get 'top' => 'top'
-  get 'show' => 'books#show'
-  post 'new' => 'books#new'
+end
+
